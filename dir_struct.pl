07a494fd1dd8206ec3237e2eccb91fe26a1bb0f0
Jon Larsen <github@jonathanlarsen.com>
2014-09-01 03:22:08 -0400

    added working dir_structure script

--- dir_struct.pl
+++ dir_struct.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use File::Path;
 
 my $base_dir = "/opt/boopsie/test";
 my $file = "/tmp/boopsie_challenge/boopsie_test.tsv";
+
 open (my $fh, $file)
   or die "Could not open file '$file' $!";
-
 while ( my $row = <$fh> ) {
 	chomp $row;
 	my @array = split("\t", $row);
-	mkdir -p "/opt/boopsie/test/$array[2]/$array[3]";
-	print "$base_dir/$array[2]/$array[3]\n";
-	#unless(open FILE, '>'.$file) {
-	#	die "Couldn't do it, Cap'n. $!\n";
-	#}
-	#print FILE $array[4];
-	#print "$array[0]\n";
+	
+	unless (-d "$base_dir/$array[2]/$array[3]") {
+		mkpath("$base_dir/$array[2]/$array[3]") or die "Error: $!";
+		}
+
+	unless(open FILE, '>'."$base_dir/$array[2]/$array[3]/$array[0].txt") {
+		die "Couldn't do it, Cap'n. $!\n";
+	}
+	print FILE $array[4];
 }
