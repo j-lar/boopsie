b8aaeaba40f8a9dc929ed906e35df8f279ae11c3
Jon Larsen <github@jonathanlarsen.com>
2014-09-01 00:50:30 -0400

    added script to check variant and install

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rh = "yum -y install ffmpeg, vim, tree, screen, netcat"
+deb = "apt-get -y install ffmpeg, vim, tree, screen, netcat"
+
+## install ffmpeg, vim, tree, screen, netcat based on OS (debian or redhat)
+echo "Checking distro family..."
+if (which yum) | grep -q "not found"
+then
+	echo "Looks like a Red Hat variant. Running yum."
+	$rh
+else 
+	echo "Looks like a Debian variant. Running apt-get."
+	$deb 
+fi
