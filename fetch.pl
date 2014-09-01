fe21af0840a880db7f6309cd4cd69cf2fa6c6927
Jon Larsen <github@jonathanlarsen.com>
2014-09-01 01:47:32 -0400

    removed ip check

--- fetch.pl
+++ fetch.pl
@@ -2,31 +2,20 @@
 use strict;
 use warnings;
 use File::Fetch;
-use LWP::Simple
 
-my $domain = 'www.boopsie.com';
-my $url = 'http://public.boopsie.com';
-my $path = '_test/boopsie_test.tsv'
-my $dir = glob('~/boopsie_challenge');
-
-# get IP address of public.boopsie.com (combine with download and check if up?)
-
-my $ip = exec(dig +short $url);
-
-## detect if www.boopsie.com is up. verify it's not a dns issue by check without www
-
-if (! head($url)) {
-  die "$url is not reachable."
-}
-else {
-  print "$url is up!";
-}
-
+my $domain = 'public.boopsie.com';
+#my $url = 'http://public.boopsie.com/_test/boopsie_test.tsv';
+my $url = 'http://kallisti.sacredchao.us/_test/boopsie_test.tsv';
+my $path = '_test/boopsie_test.tsv';
+my $dir = glob('/tmp/boopsie_challenge');
 
 # create directory for all the stuff we'll be doing
-unless(mkdir $dir) {
-  die "Couldn't create base directory\n$!\n"
+if ( ! -d $dir ) {
+  unless(mkdir $dir) {
+    die "Couldn't create base directory\n$!\n";
+  }
 }
 
-my $download = File::Fetch->new(uri => '$url/$path' );
+# downlad to the dir we created
+my $download = File::Fetch->new(uri => $url );
 my $fetcher = $download->fetch ( to => $dir) or die "failed... $download->error";
