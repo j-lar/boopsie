f07ed901e41b985ef4cb9ea22ce23bd2869bc3ea
Jon Larsen <github@jonathanlarsen.com>
2014-08-31 23:40:49 -0400

    added section to import tsv file into mysql db

--- create_db.pl
+++ create_db.pl
@@ -14,6 +14,7 @@ my $user = "root";
 my $database = "libraries";
 my $dbh = DBI -> connect("dbi:mysql::$host",$user,$password);
 my $file = glob('~/boopsie_challenge/boopsie_test.tsv');
+my $table_name = "info";
 
 open (FILE, $file)
   or die "Could not open $file $!";
@@ -26,5 +27,13 @@ my @schema = split("\t", $header);
 $dbh -> do("DROP DATABASE IF EXISTS `$database`;");
 $dbh -> do("CREATE DATABASE `$database`;");
 $dbh = DBI -> connect("dbi:mysql:dbname=$database:$host",$user,$password);
-$dbh -> do("CREATE TABLE info ($schema[0] VARCHAR(256), $schema[1] VARCHAR(256), $schema[2] VARCHAR(8), $schema[3] VARCHAR(10), $schema[4] VARCHAR(256));");
+$dbh -> do("CREATE TABLE $table_name ($schema[0] VARCHAR(256), $schema[1] VARCHAR(256), $schema[2] VARCHAR(8), $schema[3] VARCHAR(10), $schema[4] VARCHAR(256));");
 
+#import from boopsie_test.tsv into db, ignoring first line
+$dbh -> do(LOAD DATA LOCAL INFILE $file
+		   INTO TABLE $table_name
+		   FIELDS TERMINATED by '\t'
+		   LINES TERMINATED BY '\n'
+		   IGNORE 1 LINES
+		   ($schema[0], $schema[1], $schema[2], $schema[3], $schema[4]);
+		  );
