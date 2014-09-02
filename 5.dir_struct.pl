28442cdcf1c0e2e08189e0fd2660995ce326ac89
Jon Larsen <github@jonathanlarsen.com>
2014-09-02 15:03:49 -0400

    check for sudo, and replace '/' in library names with '-'

--- 5.dir_struct.pl
+++ 5.dir_struct.pl
@@ -6,6 +6,11 @@ use File::Path;
 my $base_dir = "/opt/boopsie/test";
 my $file = "/tmp/boopsie_challenge/boopsie_test.tsv";
 
+# Check for root access
+if ($<) {
+	die "Use sudo to execute this script, or directory creation will fail.\n";
+}
+
 open (my $fh, $file)
   or die "Could not open file '$file' $!";
 while ( my $row = <$fh> ) {
@@ -14,10 +19,14 @@ while ( my $row = <$fh> ) {
 	
 	unless (-d "$base_dir/$array[2]/$array[3]") {
 		mkpath("$base_dir/$array[2]/$array[3]") or die "Error: $!";
-		}
+	}
+# need to check for '/' in library name
+	for ($array[0]) {
+		s/\//\-/g
+	}
 
 	unless(open FILE, '>'."$base_dir/$array[2]/$array[3]/$array[0].txt") {
-		die "Couldn't do it, Cap'n. $!\n";
+		die "Couldn't open $base_dir/$array[2]/$array[3]/$array[0].txt $!\n";
 	}
 	print FILE $array[4];
 }
