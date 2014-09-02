5d86664b94d77076ba8678bcabe8eb73e4bbf6e7
Jon Larsen <github@jonathanlarsen.com>
2014-09-01 21:01:55 -0400

    working tcpdump install and test script

--- tcpdump_install.sh
+++ tcpdump_install.sh
@@ -1,20 +1,59 @@
 #!/bin/bash
+blue=$(tput setaf 4)
+normal=$(tput sgr0)
 
-apt-get install flex
-apt-get install byacc
+printf "${blue}Going to run apt-get in case we need to install flex and byacc.\nGet ready to enter sudo password}\nPress Enter when ready.${normal}"
+read
+
+#install prereqs for building if necessary
+sudo apt-get install flex byacc -y
+
+#download, extract, configure and make latest version of libpcap
+printf  "${blue}Ready to download and install latest version of libpcap. Press enter when ready${normal}"
+read
 wget http://www.tcpdump.org/release/libpcap-1.6.1.tar.gz
-cd libpcap-1.6.1
-sh configure
-wget http://www.tcpdump.org/release/tcpdump­4.6.1.tar.gz
-tar -xvzf tcpdump­-4.6.1.tar.gz
-cd tcpdump­-4.6.1
-sh configure
+tar -xvzf libpcap-1.6.1.tar.gz
+
+#change to libpcap make directory
+pushd libpcap-1.6.1
+sh ~/libpcap-1.6.1/configure
 make
-if sh tests/TESTrun.sh | grep -v '^.*\s0.*failed.*$'
-	then echo "Tests ran successfully."
+popd
+
+#download, extract latest version of tcpdump
+printf  "${blue}Ready to download and install latest version of tcpdump. Press enter when ready.${normal}"
+read
+wget http://www.tcpdump.org/release/tcpdump-4.6.1.tar.gz
+tar -xvzf ~/tcpdump-4.6.1.tar.gz
+
+#change to tcpdump make directory
+pushd ~/tcpdump-4.6.1
+sh ~/tcpdump-4.6.1/configure
+make
+popd
+
+
+printf "\n\n${blue}Ready to run tests. Press Enter to continue.${normal}\n\n"
+read
+
+#change to test directory and run tests
+pushd ~/tcpdump-4.6.1/tests
+if ~/tcpdump-4.6.1/tests/TESTrun.sh | grep '^.*\s0.*failed.*$'
+        then printf "\n\n${blue}Tests ran successfully. Press Enter to continue.${normal}\n\n"
+        read
+else
+        printf "\n\n${blue}Tests failed. Something is wrong and you should figure that out.${normal}\n\n" && exit 0
 fi
+popd
+
+#install and go back to original dir
+
+pushd ~/tcpdump-4.6.1
 sudo make install
+popd
+
 ### run tcpdump for something to test it.
-tcpdump -w test.pcap -i eth1 tcp port 22
-if [ -s /tmp/f1 ]
-	then echo "tcpdump is working!"
+sudo timeout --foreground 10s tcpdump -w test.pcap
+if [ -s test.pcap ]
+        then printf "\n\n${blue}tcpdump is working!${normal}\n\n"
+fi
